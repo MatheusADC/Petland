08b9c08141875d44cd2b66065bee9c076b5034c8
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2026-01-13 09:10:04 -0300

    feat: CadastroRepository em StartApp

--- Petland/petland-api/petland-api/src/main/java/com/petland/start/StartApp.java
+++ Petland/petland-api/petland-api/src/main/java/com/petland/start/StartApp.java
@@ -1,14 +1,24 @@
 package com.petland.start;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import com.petland.model.Cadastro;
+import com.petland.repository.CadastroRepository;
+
 @Component
 public class StartApp implements ApplicationRunner {
+
+    @Autowired
+    private CadastroRepository repository;
     
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Testando um método que será executado pelo spring");
+        Cadastro matheus = new Cadastro();
+        matheus.setId(2);
+        matheus.setNome("Matheus");
+        repository.save(matheus);
     }
 }
