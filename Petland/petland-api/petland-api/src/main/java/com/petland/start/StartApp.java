71d4e7fed7e58c649e301d4a4f0d0848fe557e6b
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2026-02-01 10:08:50 -0300

    feat: Método de execução

--- Petland/petland-api/petland-api/src/main/java/com/petland/start/StartApp.java
+++ Petland/petland-api/petland-api/src/main/java/com/petland/start/StartApp.java
@@ -5,24 +5,19 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import com.petland.model.Cadastro;
-import com.petland.model.Endereco;
-import com.petland.model.Perfil;
-import com.petland.repository.CadastroRepository;
+import com.petland.cadastros.repository.AnimalRepository;
+import com.petland.cadastros.repository.CadastroRepository;
 
 @Component
 public class StartApp implements ApplicationRunner {
 
     @Autowired
-    private AnimalRepository repository;
+    private CadastroRepository cadastroRepository;
+
+    @Autowired
+    private AnimalRepository animalRepository;
     
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Animal garfield = new Animal();
-        garfield.setNome("Garfield");
-        garfield.setAniversario(LocalDate.of(1978, 06, 19));
-        garfield.setEspecie(AnimalEspecie.GATO);
-
-        repository.save(garfield);
     }
 }
